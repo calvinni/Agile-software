df7dfbbd50b204e67f2c550c23b4863c3ed8e5e7
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-10 04:47:11 +0800

    Create dbh.php

---
+++ dbh.php
@@ -0,0 +1,17 @@
+<?php
+//setting up database
+
+//local connection: 
+ $servername = "localhost";
+ $dbUsername = "root";
+ $dbPassword = "";
+ $dbName = "useraccounts";
+
+$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
+
+if(!$conn)
+{
+    die("Connection failed: ".mysqli_connect_error());
+}
+
+?>
