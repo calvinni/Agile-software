d7894521382e09b7f3c4b2099092a6d3fb3078f0
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-10 04:42:42 +0800

    Create Register.php

---
+++ Register.php
@@ -0,0 +1,65 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+<?php
+    if(isset($_GET['error'])) //<!-- Checking the error that we wrote in URL -->
+            { 
+                if($_GET['error'] == 'usertaken')
+                    echo '<p class="">Mobile already registered. Please change your mobile Number.</p>';
+                else if($_GET['error'] == 'passwordinvalid')
+                    echo '<p class="">Password and Confirm Password does not match. Please try again.</p>';
+                else if($_GET['error'] == 'sqlerror')
+                    echo '<p class="">Unable to register, Please check your connection and try again</p>';
+                else if($_GET['error'] == 'sqlerror2')
+                    echo '<p class="">Unable to register, Please check your connection and try again</p>';
+            }
+            else if(isset($_GET['signup']))
+                if($_GET['signup'] == "success")
+                    echo '<p class="">Sign up successfull! Click <a href="Login.php">here</a> to login.</p>';
+?>
+<head>
+    <title>Registeration Page</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+
+<body>
+    <!-- Nav Bar -->
+    <section class="nav">
+        <a href="/" class="recircle" style="float: left;"><b>ReCircle</b></a>
+        <section class="protectedSection">
+            <a href="/cart"><b>Cart</b></a>|
+            <a href="/orderHistory"><b>Order History</b></a>|
+            <a href="/profile"><b>Profile</b></a></div>|
+            <a class="logout"><b>Logout</b></a>
+        </section>
+        <section class="unprotectedSection">
+            <a href="login.html"><b>Login</b></a>|
+            <a href="register.html"><b>Register</b></a>
+            <a href="locateUs.html"><b>Locate Us</b></a>
+            <a href="rewards.html"><b>rewards</b></a>
+            <a href="FAQ.html"><b>FAQs</b></a>
+        </section>   
+    </section>
+    <!-- End Of Nav Bar -->
+    <section>
+        <br>
+     <h1>Register</h1>
+        <form method="post" action="checkregister.php">
+            <label for="name">Username:</label>
+            <input type="text" name="username" id = "username" required>
+            <br>
+            <label for="contact">Mobile Number:</label>
+            <input type="number" id="contact" name="contact" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <br>
+            <label for="password">Confirm password:</label>
+            <input type="password" id="confpw" name="confpw" required>
+            <br>
+            <button role="submit" name="signup_submit">Create Account</button>
+        </form>
+    </section>
+</body>
+
+</html>
