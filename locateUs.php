fa326a890f02f73ebfc70c17392393b3d86ad580
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-10 04:50:55 +0800

    Create locateUs.php

---
+++ locateUs.php
@@ -0,0 +1,35 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Locate Us Page</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+
+<body>
+
+    <section class="nav">
+        <a href="/" class="recircle" style="float: left;"><b>ReCircle</b></a>
+        <section class="protectedSection">
+            <a href="/cart"><b>Cart</b></a>|
+            <a href="/orderHistory"><b>Order History</b></a>|
+            <a href="/profile"><b>Profile</b></a></div>|
+            <a class="logout"><b>Logout</b></a>
+        </section>
+        <section class="unprotectedSection">
+            <a href="login.html"><b>Login</b></a>|
+            <a href="register.html"><b>Register</b></a>
+            <a href="locateUs.html"><b>Locate Us</b></a>
+            <a href="rewards.html"><b>rewards</b></a>
+            <a href="FAQ.html"><b>FAQs</b></a>
+        </section>
+        
+    </section>
+
+    <h1>Locate Us Page</h1>
+    <p>collection point addresses</p>
+    
+</body>
+
+</html>
