ef5cb1bce7ac77504566aa0aa7c5ba0cacd2d255
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-10 04:51:22 +0800

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,34 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Profile Page</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+
+<body>
+
+    <section class="nav">
+        <a href="/" class="recircle" style="float: left;"><b>ReCircle</b></a>
+        <section class="protectedSection">
+            <a href="/cart"><b>Cart</b></a>|
+            <a href="/orderHistory"><b>Order History</b></a>|
+            <a href="/profile"><b>Profile</b></a></div>|
+            <a class="logout"><b>Logout</b></a>
+        </section>
+        <section class="unprotectedSection">
+            <a href="login.html"><b>Login</b></a>|
+            <a href="register.html"><b>Register</b></a>
+            <a href="locateUs.html"><b>Locate Us</b></a>
+            <a href="rewards.html"><b>rewards</b></a>
+            <a href="FAQ.html"><b>FAQs</b></a>
+        </section>
+        
+    </section>
+
+    <h1>Profile</h1>
+    
+</body>
+
+</html>
