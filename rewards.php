a92eb4e1b245c23b36def17c2a32f223d377af9a
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-10 04:52:34 +0800

    Create rewards.php

---
+++ rewards.php
@@ -0,0 +1,35 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Rewards Page</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+
+<body>
+
+    <section class="nav">
+        <a href="/" class="recircle" style="float: left;"><b>ReCircle</b></a>
+        <section class="protectedSection">
+            <a href="/cart"><b>Cart</b></a>|
+            <a href="/orderHistory"><b>Order History</b></a>|
+            <a href="/profile"><b>Profile</b></a></div>|
+            <a class="logout"><b>Logout</b></a>
+        </section>
+        <section class="unprotectedSection">
+            <a href="login.html"><b>Login</b></a>|
+            <a href="register.html"><b>Register</b></a>
+            <a href="locateUs.html"><b>Locate Us</b></a>
+            <a href="rewards.html"><b>rewards</b></a>
+            <a href="FAQ.html"><b>FAQs</b></a>
+        </section>
+        
+    </section>
+
+    <h1>Rewards</h1>
+    <p> In this page, display the points reference eg 100pts can redeem $10 ntuc voucher</p>
+    
+</body>
+
+</html>
