d0af3fc3b65bc69a97c05a56001fb930caa50346
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-10 04:45:18 +0800

    Create checklogin.php

---
+++ checklogin.php
@@ -0,0 +1,75 @@
+<?php
+
+if(isset($_POST['login_submit'])) //checking if user got here from submit button and not from URL
+{
+    require 'dbh.php';
+
+    $mobile = $_POST['Mobile'];
+    $password = $_POST['Password'];
+
+    if(empty($mobile) || empty($password))
+    {
+        header("Location: ../Login.php?error=emptyfields");
+        exit();
+    }
+    else 
+    {
+        $sql = "SELECT * FROM users WHERE Mobile=?";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)) //checking if $sql is good to go with $stmt, prepare statement
+        { 
+            header("Location: ../home.php?error=sqlerror");
+            exit();
+        }
+        else //grab info we got from $sql
+        { 
+            mysqli_stmt_bind_param($stmt, "s", $mobile); //include statement that we want to send ($stmt) + tell what kind of data sent + actual data to bind
+    
+            mysqli_stmt_execute($stmt); //executing previous params
+    
+            $result = mysqli_stmt_get_result($stmt); //info we got from db are now in $result (matches?)
+    
+            if($row = mysqli_fetch_assoc($result))//storing $result into an associative array to allow php manipulation
+            {
+                $pwdCheck = password_verify($password, $row['Password']); //checking if user entered pwd is same as the one in db. (will hash the user input pwd prior)
+        
+                if($pwdCheck == false)
+                {
+                    //echo $mobile;   //testing purpose
+                    //echo $password; //testing purpose
+                    echo $row['Password'];
+                    var_dump($row); //show array
+                    //header("Location: ../Login.php?error=wrongpwd");
+                    exit();
+                }
+                else if($pwdCheck == true) //we want lock in user if success login: need session, global variable that has info of user.
+                {
+                    session_start();
+                    $_SESSION['userId'] = $row['ID'];
+                    $_SESSION['userName'] = $row['Username'];
+
+                    header("Location: ../home.php?login=success");
+                    exit();
+                }
+                else
+                {
+                    //echo $mobile;   //testing purpose
+                    //echo $password; //testing purpose
+                    //header("Location: ../Login.php?error=wrongpwd");
+                    exit();
+                }
+            }
+            else //if $result is empty
+            {
+                header("Location: ../Login.php?error=emailDNE");
+                exit();
+            }
+    }
+}
+}
+else {
+    header("Location: ../Login.php");
+    exit();
+}
+
+?>
