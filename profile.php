546adef074d7dcff6a8681d11f552757fc41b6d7
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-10 15:35:43 +0800

    changed nav

--- profile.php
+++ profile.php
@@ -10,7 +10,7 @@
 <body>
 
     <section class="nav">
-        <a href="/" class="recircle" style="float: left;"><b>ReCircle</b></a>
+        <a href="/" class="recircle" style="float: left;"><b>ReCircle</b></a>|
         <section class="protectedSection">
             <a href="/cart"><b>Cart</b></a>|
             <a href="/orderHistory"><b>Order History</b></a>|
@@ -18,13 +18,12 @@
             <a class="logout"><b>Logout</b></a>
         </section>
         <section class="unprotectedSection">
-            <a href="login.html"><b>Login</b></a>|
-            <a href="register.html"><b>Register</b></a>
-            <a href="locateUs.html"><b>Locate Us</b></a>
-            <a href="rewards.html"><b>rewards</b></a>
-            <a href="FAQ.html"><b>FAQs</b></a>
+            <a href="Login.php"><b>Login</b></a>|
+            <a href="Register.php"><b>Register</b></a>
+            <a href="locateUs.php"><b>Locate Us</b></a>
+            <a href="rewards.php"><b>rewards</b></a>
+            <a href="FAQ.php"><b>FAQs</b></a>
         </section>
-        
     </section>
 
     <h1>Profile</h1>
