b2807b7516c2212c7a5a393cccbb653d85157afb
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-17 15:59:22 +0800

    added form

--- rewards.php
+++ rewards.php
@@ -3,31 +3,74 @@
 <html lang="en">
 
 <head>
-    <title>Rewards Page</title>
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recircle Team 81</title>
+    <!--bootstrap css link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- font awesome link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" 
+    integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
 </head>
-
+<!-- this page includes about us, how to recycle using our website, what we collect -->
 <body>
-
-   <section class="nav">
-        <a href="/" class="recircle" style="float: left;"><b>ReCircle</b></a>|
-        <section class="protectedSection">
-            <a href="/cart"><b>Cart</b></a>|
-            <a href="/orderHistory"><b>Order History</b></a>|
-            <a href="/profile"><b>Profile</b></a></div>|
-            <a class="logout"><b>Logout</b></a>
-        </section>
-        <section class="unprotectedSection">
-            <a href="Login.php"><b>Login</b></a>|
-            <a href="Register.php"><b>Register</b></a>
-            <a href="locateUs.php"><b>Locate Us</b></a>
-            <a href="rewards.php"><b>rewards</b></a>
-            <a href="FAQ.php"><b>FAQs</b></a>
-        </section>
-    </section>
+    <!-- nav bar -->
+    <div class = "container-nav">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/index.php"><i class="fa-solid fa-recycle"></i> ReCircle</a>
+                <!-- extend button -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./locateUs.php">Locate Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./rewards.php">Rewards</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./faqs.php">FAQs</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-solid fa-user"></i>   
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="./login.php">Login</a></li>
+                                <li><a class="dropdown-item" href="./register.php">Register</a></li>
+                            </ul>
+                            
+                        </li>
+    
+                    </ul>
+    <!-- end of nav bar -->
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
 
     <h1>Rewards</h1>
-    <p> In this page, display the points reference eg 100pts can redeem $10 ntuc voucher</p>
+    <h3> Use your points to redeem vouchers! </h3>
+    <form id="form_voucher" name="form_voucher" method="post" action="">
+            <label for="thousand">1000 points for $10 voucher</label>
+            <br>
+            <button type="submit" name="voucher_10">Redeem</button>
+        </form>
+    
     
 </body>
 
