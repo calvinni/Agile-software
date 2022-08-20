e85e4ce56de39523dd74420ccfdadfbb7516c240
Calvin_130 <109656337+calvinni@users.noreply.github.com>
2022-08-20 17:29:48 +0800

    updated to yangtings work

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                                         <a class="nav-link" href="./rewards.php">Rewards</a>
                                        </li>';
                              }
-                            ?>
+                        ?>
                         <li class="nav-item">
                             <a class="nav-link" href="./faqs.php">FAQs</a>
                         </li>
@@ -49,14 +49,11 @@
                             <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="fa-solid fa-user"></i>   
                             </a>
-                            <ul class="dropdown-menu">
+                            <ul class="dropdown-menu"> hhahaha
                                 <?php 
                                     if(isset($_SESSION['userId']))
                                     {
-                                        $name = $_SESSION['userName'];
-                                        echo '<div class="login_message">Hi '.$name.',<br>You are logged in!</div>
-                                        <br>
-                                        <li><a class="dropdown-item" href="./logout.php">logout</a></li>';
+                                      echo '<li><a class="dropdown-item" href="./logout.php">logout</a></li>';
                                     }
                                     else 
                                     {
@@ -67,8 +64,15 @@
                             </ul>
                             
                         </li>
-
-                        
+                        <?php 
+                                if(isset($_SESSION['userId']))
+                                {
+                                    $name = $_SESSION['userName'];
+                                    echo '<li class="nav-item">
+                                            <div class="login_message">Hi '.$name.',<br>You are logged in!</div>
+                                          </li>';
+                                }
+                        ?>
                     </ul>
 
                     <form class="d-flex" role="search">
@@ -79,58 +83,192 @@
             </div>
         </nav>
     </div>
-<!-- home slider -->
-   <!-- <section class="home-slider">
-    <p>a slider display recycle articles</p>
-    <button type="button">a button click to read more</button>
-   </section> -->
-   <div id="carouselControls" class="carousel slide" data-bs-ride="carousel">
+<!-- about us slider -->
+
+</div>
+   <div id="carouselAboutus" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="./images/10181.jpg" height="600" width="100%" alt="1" >
+                <img src="./images/index-aboutus1.png" width="100%" alt="1" >
             </div>
             <div class="carousel-item">
-                <img src="./images/Garbage recycle bins set.jpg" height="600" width="100%" alt="2">
+                <img src="./images/index-aboutus2.png" width="100%" alt="2">
             </div>
             <div class="carousel-item">
-                <img src="./images/Garbage sorting set.jpg" height="600" width="100%" alt="3">
+                <img src="./images/index-aboutus3.png" width="100%" alt="3">
             </div>
         </div>
+
    </div>
+
+<!-- how to recycle slider -->
+
+
 
 <!-- categories slider -->
 <div id="catcarouselCaptions" class="carousel slide" data-bs-ride="false">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#catcarouselCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#catcarouselCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#catcarouselCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#catcarouselCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="./images/utilization-empty-water-bottle.jpg" height="600" width="100%" alt="plastic bottle">
+      <img src="./images/index-wwc1.png" width="100%" alt="plastic bottle">
       <div class="carousel-caption d-none d-md-block">
         <h5>What we collect</h5>
         <p>Plastic bottles</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="./images/recycling-concept-flat-lay.jpg" height="600" width="100%" alt="paper">
+      <img src="./images/index-wwc2.png" width="100%" alt="paper">
       <div class="carousel-caption d-none d-md-block">
         <h5>What we collect</h5>
         <p>Recycle papers</p>
       </div>
     </div>
-    
+    <div class="carousel-item">
+      <img src="./images/index-wwc3.png" width="100%" alt="can">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>What we collect</h5>
+        <p>Can</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./images/index-wwc4.png" width="100%" alt="glasses">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>What we collect</h5>
+        <p>Glass</p>
+      </div>
+    </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="catcarouselCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="catcarouselCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-white text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 link-grayish">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-grayish">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-grayish">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 link-grayish">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+          <i class="fa-solid fa-recycle"></i>ReCircle
+          </h6>
+          <p>
+            Let's start Recycle!
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Recycle
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Bottles</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Can</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Glass</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Paper</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">FAQs</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Rewards</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Register</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3 text-grayish"></i> ABC 123 Road Singapore 123456 Singapore</p>
+          <p>
+            <i class="fas fa-envelope me-3 text-grayish"></i>
+            recirclesg@example.com
+          </p>
+          <p><i class="fas fa-phone me-3 text-grayish"></i> + 65 1234 5678</p>
+          <p><i class="fas fa-print me-3 text-grayish"></i> + 65 1234 5678</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">ReCircle.com.sg</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
    
 
 
