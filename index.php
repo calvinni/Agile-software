f847904f753815687c1bf390f5ad0abe4854a149
Calvin_130 <calvinyeo130@gmail.com>
2022-08-23 19:15:16 +0800

    added css for login

--- index.php
+++ index.php
@@ -77,11 +77,12 @@
                                 }
                         ?>
                         
-                    </li>
+                    
                     <form class="d-flex" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
+                    </li>
                 </div>
             </div>
         </nav>
