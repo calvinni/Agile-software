f4c6ba15a3fe59733a4f3dbfac94ebcff7227a37
Calvin_130 <calvinyeo130@gmail.com>
2022-08-25 23:12:01 +0800

    bug fixes

--- checkout.php
+++ checkout.php
@@ -1,21 +1,19 @@
 <?php
-/*validates user input, checks if mobile already exists, inserts new input info into db*/
 
 if (isset($_POST["checkout"])) //checking if came here from click submit
 {
     require 'dbh.php';  //using the $conn variable
 
-    //storing input values from html submit
     $UserID = $_POST['UID'];
     
-    $sql = "DELETE FROM cart WHERE cart_id = '$UID' ";
-    $result = mysqli_query($conn, $sql);
+    $Query = "DELETE FROM cart WHERE cart_id='$UID'";
+    $result = mysqli_query($conn, $Query);
 
     header("Location: ../order.php?checkout=success");
 }
 else
 {
-    header("Location: ../order.php"); //if user didnt come from 'submit', return to sign up
+    header("Location: ../order.php"); //if user didnt come from 'checkout', return to sign up
     exit();
 }
 
