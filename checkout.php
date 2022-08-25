f9841b8790a2c2e94f9db62d45d5dc480693ce7b
Calvin_130 <calvinyeo130@gmail.com>
2022-08-25 23:19:35 +0800

    checkout changed variables

--- checkout.php
+++ checkout.php
@@ -1,19 +1,19 @@
 <?php
 
-if (isset($_POST["checkout"])) //checking if came here from click submit
+if(isset($_POST["checkout"])) //checking if came here from click submit
 {
     require 'dbh.php';  //using the $conn variable
 
     $UserID = $_POST['UID'];
     
-    $Query = "DELETE FROM cart WHERE cart_id='$UID'";
+    $Query = "DELETE FROM cart WHERE cart_id='$UserID'";
     $result = mysqli_query($conn, $Query);
 
-    header("Location: ../order.php?checkout=success");
+    header("Location: ../cart.php?checkout=success");
 }
 else
 {
-    header("Location: ../order.php"); //if user didnt come from 'checkout', return to sign up
+    header("Location: ../cart.php"); //if user didnt come from 'checkout', return to sign up
     exit();
 }
 
