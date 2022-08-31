eda5c19f5eadd5e531543acb2031fc3d84f69ca7
Calvin_130 <calvinyeo130@gmail.com>
2022-08-31 20:31:12 +0800

    changed delete to delete and redirect

--- changecart.php
+++ changecart.php
@@ -3,10 +3,9 @@ require 'dbh.php';  //using the $conn variable
 $Order_Name = $_POST['OrderName'];
 $Order_Quantity = $_POST['OrderQuantity'];
 $_ID = $_POST['I_D'];
-echo $Order_Name;
-echo $Order_Quantity;
-echo $_ID;
+?>
 
+<?php
 if (isset($_POST["edit"])) //checking if came here from click submit
 {
 ?>
@@ -42,36 +41,11 @@ if (isset($_POST["edit"])) //checking if came here from click submit
 }
 else if (isset($_POST["delete"]))
 {
-?>
-    <section>
-    <h1>Delete Order</h1>
-    <div class="wrapper">
-        <h2>Edit your order here</h2>
-        <!-- pull the data from sql -->
-        <form action="changecart.php" method="POST">
-            <p>Key in the recycle items name that need to be added.</p>
-            <select id="OrderName" name="OrderName">
-                <option value="paper" <?php if ($Order_Name = 'Paper') {echo 'selected';}?>>Paper</option>
-                <option value="plastic" <?php if ($Order_Name = 'Plastic') {echo 'selected';}?>>Plastic</option>
-                <option value="metal" <?php if ($Order_Name = 'Metal') {echo 'selected';}?>>Metal</option>
-                <option value="glass" <?php if ($Order_Name = 'Glass') {echo 'selected';}?>>Glass</option>
-            </select>
-            <p>
-            <p>Key in the quantity in KG that need to be added.</p>
-                <input id="OrderQuantity" name="OrderQuantity" type="number" value="<?php echo $Order_Quantity; ?>" required>
-            <p></p>
-            <button type="submit" name="Editing">Edit</button>
-        </form>
-        <!-- End Of sql -->
-        <?php
-          if (isset($_POST['Editing'])) 
-          {
-            echo '<p class="">successfully edited</p>'; //temp
-          }
-        ?>
-    </section>
-  </div>
-<?php    
+    $Query = "DELETE FROM cart WHERE ID='$_ID'";
+    $result = mysqli_query($conn, $Query);
+
+    header("Location: ../cart.php?delete=success");
+    exit();
 }
 else
 {
