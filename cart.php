f18ea60aa54c27cbad16237e006aedca907885e9
Calvin_130 <calvinyeo130@gmail.com>
2022-09-02 17:02:36 +0800

    added the js

--- cart.php
+++ cart.php
@@ -350,5 +350,6 @@ if ($resultCheck > 0)
 
     <!-- bootstrap js link-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="hide.js"></script>
 </body>
 </html>
