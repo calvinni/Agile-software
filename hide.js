d0ce0605d67dd20fa76f4d98d06be306670492b3
Calvin_130 <calvinyeo130@gmail.com>
2022-09-02 17:08:44 +0800

    set it to hide

--- hide.js
+++ hide.js
@@ -4,13 +4,12 @@ btn.addEventListener('click', () =>
 {
   const form = document.getElementById('resForm');
 
-  if (form.style.visibility === 'hidden') 
-  {
-    form.style.visibility = 'visible';
-  } 
-  else 
-  {
-    form.style.visibility = 'hidden';
+  if (form.style.display === 'none') {
+    // 👇️ this SHOWS the form
+    form.style.display = 'block';
+  } else {
+    // 👇️ this HIDES the form
+    form.style.display = 'none';
   }
 }
     );
